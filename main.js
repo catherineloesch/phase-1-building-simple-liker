5ea7ffeeeae6c8c70e87741432484472b88d1083
Catherine Loesch <catherine.loesch@protonmail.com>
2021-06-21 23:48:44 +0100

    completed assignment

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const hearts = document.getElementsByClassName("like-glyph")
+const errorModal = document.getElementById("modal")
+const modalMessage = document.getElementById("modal-message")
 
 // Your JavaScript code goes here!
+// functions
+function likes(event){
+  mimicServerCall()
+  .then(function(resp) {
+    console.log(resp)
+    if (event.target.innerHTML === EMPTY_HEART){
+      event.target.innerHTML = FULL_HEART
+      event.target.classList.add("activated-heart")
+    } else {
+      event.target.innerHTML = EMPTY_HEART
+      event.target.classList.remove("activated-heart")
+    }
+  })
+  .catch((error) => {
+    modalMessage.innerHTML = "Random server error. Try again."
+    errorModal.classList.remove("hidden")
+    setTimeout(() => errorModal.classList.add("hidden"), 3000)
+  }) 
+}
 
+// event listener
+for (const heart of hearts){
+  heart.addEventListener("click", likes)
+}
 
 
 
